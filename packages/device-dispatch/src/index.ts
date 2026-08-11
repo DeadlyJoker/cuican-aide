882be96a3ffc50c3eaa8a2c9e7cdb6efc97f0b1a
wangqichen <1121620429@qq.com>
2026-08-11 13:12:39 +0800

    feat(device-dispatch): export workspace dispatch

--- packages/device-dispatch/src/index.ts
+++ packages/device-dispatch/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./device-tool-runtime.ts";
 export * from "./ed25519-device-command-signer.ts";
+export * from "./ed25519-device-workspace-command-signer.ts";
 export * from "./https-device-dispatch-client.ts";
+export * from "./workspace-list-executor.ts";
+export * from "./workspace-list-dispatch-client.ts";
