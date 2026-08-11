54cb89c51f2aba6b29186111bb75e451f6d6b995
wangqichen <1121620429@qq.com>
2026-08-11 13:22:12 +0800

    test(control-client): cover workspace integration

--- packages/control-client/src/index.ts
+++ packages/control-client/src/index.ts
@@ -2,3 +2,4 @@ export * from "./control-api-client.ts";
 export * from "./run-event-stream.ts";
 export * from "./thread-goal-event-stream.ts";
 export * from "./thread-event-stream.ts";
+export * from "./workspace-operation-event-stream.ts";
