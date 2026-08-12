ae0770e97019081f4e92f7f5400a2c2bcb5b6371
wangqichen <1121620429@qq.com>
2026-08-12 14:56:06 +0800

    feat(store): persist workflow DAG state

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -16,4 +16,5 @@ export * from "./postgres-run-store.ts";
 export * from "./postgres-thread-store.ts";
 export * from "./sqlite-run-store.ts";
 export * from "./workflow-version-store.ts";
+export * from "./workflow-execution-store.ts";
 export { SQLITE_SCHEMA_VERSION } from "./sqlite-schema.ts";
